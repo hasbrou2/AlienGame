1c572c1f4610ed4a28f3569d728319a76bb5fe2d
hasbrou2 <hasbrou2@outlook.com>
2021-12-24 22:21:27 -0600

    Finish game chapter

--- game_functions.py
+++ game_functions.py
@@ -123,20 +123,32 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """Respond to being hit by aliens"""
-    #Decrement ships_left
-    stats.ships_left -= 1
+    if stats.ships_left > 0:
+        #Decrement ships_left
+        stats.ships_left -= 1
 
-    #Empty the list of aliens and bullets.
-    aliens.empty()
-    bullets.empty()
+        #Empty the list of aliens and bullets.
+        aliens.empty()
+        bullets.empty()
 
-    #Create a new fleet and center the ship
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
+        #Create a new fleet and center the ship
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
-    #Pause
-    sleep(0.5)
+        #Pause
+        sleep(0.5)
+    else:
+        stats.game_active = False
 
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """Check if  any aliens have reached the bottom of the screen."""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            #Treat as same as if ship got hit
+            ship_hit( ai_settings, stats, screen, ship, aliens, bullets)
+            break
+    
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """Update the positions of all aliens in the fleet."""
     check_fleet_edges(ai_settings, aliens)
@@ -145,4 +157,6 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
         print("Ship Hit!!!")
+    """Look for aliens hitting bottom of the screen"""
+    check_aliens_bottom(ai_settings, stats, screen,ship, aliens, bullets)
         
--- game_stats.py
+++ game_stats.py
@@ -5,9 +5,13 @@ class GameStats():
         """Initialize Statistics"""
         self.ai_settings = ai_settings
         self.reset_stats()
+        #start alien invasion in an active state
+        self.game_active = True
 
     def reset_stats(self):
         """Initialize Statistics that can change during the game"""
         self.ships_left  = self.ai_settings.ship_limit
+
+    
 
         
